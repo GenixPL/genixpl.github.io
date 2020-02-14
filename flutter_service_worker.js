1fc4ddfe0193dd07581f5f4386f966e191d8b637
genix <intergenixyt@gmail.com>
2020-02-14 15:16:32 +0100

    ver 3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,17 +2,18 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "abb72016b8d1a019345deb6e95124f00",
-"/main.dart.js": "5edddfcbbe8f793296a1073ed6e0521e",
+"/main.dart.js": "92a00dcc45dc8d1edf1d03f39e451b78",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/manifest.json": "0cd4c9d954c3e172e7515a3c1053e4e0",
 "/assets/LICENSE": "9f69edcc25795745c09624396a67241f",
-"/assets/AssetManifest.json": "e26ab0f0a9ebee8025260de29a88a4ca",
+"/assets/AssetManifest.json": "b8311ab0b26fe7610191c3f5cc873376",
 "/assets/FontManifest.json": "5fa2baa1355ee1ffd882bec6ab6780c7",
 "/assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "d21f791b837673851dd14f7c132ef32e",
 "/assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "bdd8d75eb9e6832ccd3117e06c51f0d3",
 "/assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "3ca122272cfac33efb09d0717efde2fa",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/assets/placelytics/logo.jpg": "2b8978bc5f75d0319dcbd6672e7d5329"
 };
 
 self.addEventListener('activate', function (event) {
