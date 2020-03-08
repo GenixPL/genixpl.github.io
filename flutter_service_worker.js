8228c43496f624068b381dc368142c55c25611f6
Łukasz Niedziałek <intergenixyt@gmail.com>
2020-03-08 22:15:02 +0100

    ver 11

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,14 +1,28 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets\AssetManifest.json": "396bb013b759b89b6645d7902b307e9f",
+  "/assets\AssetManifest.json": "d6f6fd0070c94cff5949d07ce3dafcb6",
 "/assets\assets\background.jpg": "63ae03b3ad9d8a0743d7450e784775d5",
 "/assets\assets\background_empty.jpg": "9a23165d7a36138f1eb13dc73fd928ff",
 "/assets\assets\construction.jpeg": "79dd2bc831fe08bad12e2e3a6c7a74c2",
 "/assets\assets\mailbox.jpg": "16f60cbce019b67f5ca3be217f1f08a9",
 "/assets\assets\message.jpg": "1fc6ed102afa23973c0a4f1e156fbd7a",
+"/assets\assets\mountains_1.jpg": "c1d9c9beadbb3cc1191b9afeb2965908",
+"/assets\assets\mountains_2.jpg": "148963ebf57cfb9683a3cc21cd6b4303",
 "/assets\assets\notes.jpg": "d7caec6e9290f38c620122f792d5c29e",
+"/assets\assets\pictile\logo.png": "4380ecb7b6a0a60d8a1fcb9ae9b911f5",
+"/assets\assets\pictile\screen_1.jpg": "3790c5325c82edf840673cbe763933ef",
+"/assets\assets\pictile\screen_2.jpg": "bfa843a1c3395dca31d0d7817bd46c49",
+"/assets\assets\pictile\screen_3.jpg": "de66d63a726e919d8bea3f3c029b7d27",
+"/assets\assets\pictile\screen_4.jpg": "0d2b117cd4b1c00642b728c219f215b7",
+"/assets\assets\pictile\screen_5.jpg": "a1b71bbee0a2d228b84dc9b84512394d",
+"/assets\assets\pictile\screen_6.jpg": "2b79941224f6276d8ca8dc00c410b9f8",
+"/assets\assets\pictile\screen_7.jpg": "0a9dd6f12e7be7b3cda9f4e1c0296f83",
+"/assets\assets\placelytics\logo.jpg": "2b8978bc5f75d0319dcbd6672e7d5329",
 "/assets\assets\rive_icon.png": "8907514773858755e6c846ff936237b0",
+"/assets\assets\squares.jpg": "eb8df1a62843f77fc83cdbdc3d0bedd1",
+"/assets\assets\squares_rotated.jpg": "0e0d33e2137be97f0ed39e86eb64403c",
+"/assets\assets\thg\logo.png": "6528812ec7f53704ccf0a847f194599a",
 "/assets\assets\trump.jpg": "5fef88abe280e0143044f30b0a3882fe",
 "/assets\FontManifest.json": "46c062eb21560ff9e57980926bff3dce",
 "/assets\fonts\Etna.otf": "f6617e99cf5c69aaeccbc20a2b3fc7c3",
@@ -23,7 +37,7 @@ const RESOURCES = {
 "/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "025a75f012ec686924e3a3a2fbc1cd1b",
-"/main.dart.js": "91049600b51b643a7c9afc9b3761b769",
+"/main.dart.js": "5374350c508bb66851014022c1a93d49",
 "/manifest.json": "50e07f8968f4bb98f3b1ea2f50f09f75"
 };
 
